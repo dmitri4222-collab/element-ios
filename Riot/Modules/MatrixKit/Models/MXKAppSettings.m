95fffc05e9e785b4e462427ab9c48006324027a2
dmitri4222-collab <dmitri4222@gmail.com>
2026-04-16 03:07:23 +0300

    Update MXKAppSettings.m

--- Riot/Modules/MatrixKit/Models/MXKAppSettings.m
+++ Riot/Modules/MatrixKit/Models/MXKAppSettings.m
@@ -113,7 +113,7 @@ static NSString *const kMXAppGroupID = @"group.org.matrix";
         enableBubbleComponentLinkDetection = NO;
         firstURLDetectionIgnoredHosts = @[[NSURL URLWithString:kMXMatrixDotToUrl].host];
         
-        _allowPushKitPushers = YES;
+        _allowPushKitPushers = NO;
         _notificationBodyLocalizationKey = @"MESSAGE";
         enableCallKit = YES;
         
